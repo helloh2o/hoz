ae82d536731419aa48ec7f21da04143ee67258c9
helloh2o <54695727+helloh2o@users.noreply.github.com>
2019-09-07 03:43:33 +0800

    1.fixed

--- local/client_side.go
+++ local/client_side.go
@@ -13,9 +13,8 @@ func main() {
 		log.Fatal(err)
 	}
 	s := hoz.NewServer(hoz.Config{
-		Addr: ":1080",
-		//RemoteAddr: "127.0.0.1:10800",
-		RemoteAddr: "193.110.203.47:10800",
+		Addr:       ":1080",
+		RemoteAddr: "127.0.0.1:10800",
 		Cipher:     "oor-!@adDxS$&(dl/*?",
 	})
 	go func() {
