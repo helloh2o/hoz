b3f1f1cab51787e27923d6381310ba14b3498c98
helloh2o <54695727+helloh2o@users.noreply.github.com>
2019-09-09 10:37:13 +0800

    Update oor.go

--- cipher/oor.go
+++ cipher/oor.go
@@ -5,19 +5,31 @@ import (
 )
 
 type OORR struct {
-	SecretKey   []byte
-	KeyMaxIndex int
+	password  []byte
+	index     int
+	remainder []byte
+}
+
+func NewOor(key []byte) *OORR {
+	or := new(OORR)
+	or.password = key
+	or.index = len(key) - 1
+	if or.index >= 0 {
+		for i := 0; i < 32*1024; i++ {
+			or.remainder = append(or.remainder, or.password[i%or.index])
+		}
+	}
+	return or
 }
 
 func (or *OORR) Encrypt(src []byte) ([]byte, error) {
 	length := len(src)
 	for i, b := range src {
-		if or.KeyMaxIndex >= 0 {
-			src[i] = b ^ byte(i) ^ or.SecretKey[i % or.KeyMaxIndex]
-		}else {
+		if or.index >= 0 {
+			src[i] = b ^ byte(i) ^ or.remainder[i]
+		} else {
 			src[i] = b ^ byte(i)
 		}
-
 	}
 	head := make([]byte, 4)
 	binary.BigEndian.PutUint32(head, uint32(length))
@@ -28,9 +40,9 @@ func (or *OORR) Encrypt(src []byte) ([]byte, error) {
 
 func (or *OORR) Decrypt(src []byte) ([]byte, error) {
 	for i, b := range src {
-		if or.KeyMaxIndex >= 0 {
-			src[i] = b ^ byte(i) ^ or.SecretKey[i % or.KeyMaxIndex]
-		}else {
+		if or.index >= 0 {
+			src[i] = b ^ byte(i) ^ or.remainder[i]
+		} else {
 			src[i] = b ^ byte(i)
 		}
 	}
